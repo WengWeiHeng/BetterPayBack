cb02950852412d08b51919579a208b2d2f67430d
Heng <heng@atmorning.com>
2020-07-17 20:41:38 +0900

    Branch Test..

---
+++ BetterPayBack/ViewController/BranchTestController.swift
@@ -0,0 +1,13 @@
+//
+//  BranchTestController.swift
+//  BetterPayBack
+//
+//  Created by Heng on 2020/7/17.
+//  Copyright © 2020 19cm0140. All rights reserved.
+//
+
+import UIKit
+
+class BranchTestController: UIViewController {
+    
+}
